b68c7480e5c7845520847f311549138603113606
Jukka Aho <ahojukka5@gmail.com>
2021-01-02 20:29:26 +0700

    Add unzip function

--- cmd/gontti/main.go
+++ cmd/gontti/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Gontti container util")
 	tag := os.Args[1]
+
 	fileURL := "https://github.com/" + tag + "/archive/master.zip"
 	fmt.Println("Fetching tag" + tag + " from url " + fileURL)
-
 	err := DownloadFile("master.zip", fileURL)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Downloaded file: " + fileURL)
+
+	files, err := Unzip("master.zip", "master")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Unzipped:\n" + strings.Join(files, "\n"))
 }
 
 // DownloadFile will download a url to a local file. It's efficient because it will
@@ -41,4 +51,62 @@ func DownloadFile(filepath string, url string) error {
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+// Unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+func Unzip(src string, dest string) ([]string, error) {
+
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
 }
