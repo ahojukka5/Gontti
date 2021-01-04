99f426dfe71ddd6b67eb25084301ff43d2f9b3d2
Jukka Aho <ahojukka5@gmail.com>
2021-01-04 15:10:01 +0700

    Implement function to read buffer

---
+++ cmd/gontti/reader.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+)
+
+func reader(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return
+		}
+		println("Client got:", string(buf[0:n]))
+	}
+}
