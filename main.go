e6a8d0880d8b32ed1f8a4b1eeab007dc591689e3
Jukka Aho <ahojukka5@gmail.com>
2021-01-05 01:11:55 +0700

    Add simple CLI

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/ahojukka5/gontti/utils"
+import (
+	"os"
+
+	"github.com/ahojukka5/gontti/utils"
+)
 
 func main() {
-	utils.GetVersion()
+	if len(os.Args) < 2 {
+		println("Usage: ./gontti <username/repository>")
+		return
+	}
+	repo := os.Args[1]
+	XRA := os.Getenv("DOCKER_XRA")
+	if XRA == "" {
+		println("Environment variable DOCKER_XRA not defined, unable to push image to Docker Hub.")
+		return
+	}
+	println("Building image", repo)
+	id := utils.BuildImage(repo)
+	println("Build id: ", id)
+	println("Tagging image ...")
+	utils.TagImage(id, repo)
+	println("Pushing image to Docker container registry")
+	utils.PushImage(repo, XRA)
 }
